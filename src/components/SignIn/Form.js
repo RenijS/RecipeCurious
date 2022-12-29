17363a1398f1ac919854ed6a75a4934fd232dbce
Renij Shrestha <renijshrestha8@gmail.com>
2022-12-29 13:59:23 +0545

    signIn form

--- src/components/SignIn/Form.js
+++ src/components/SignIn/Form.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Form() {
   return (
     <form className="signIn-form">
-      <h2>RecipeCurious</h2>
+      <Link to={"/"}>
+        <h2>RecipeCurious</h2>
+      </Link>
       <h1>Sign in or create an account</h1>
-      <div className="input-sect">
-        <label htmlFor="email">Email</label>
+      <div className="input-sect column">
+        <label htmlFor="email">Email address</label>
         <input type="email" name="email" id="email" />
       </div>
+      <button>Continue with Email</button>
     </form>
   );
 }
